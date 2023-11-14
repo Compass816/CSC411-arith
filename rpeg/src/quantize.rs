0c31655fb630418c795df080ba30202ae274a815
Ben Keeler <71103393+benkeeler12@users.noreply.github.com>
2023-11-13 23:53:52 -0500

    Update quantize.rs

--- rpeg/src/quantize.rs
+++ rpeg/src/quantize.rs
@@ -1,7 +1,18 @@
+
+
+/// Returns an i32.
+/// 
+/// # Arguments:
+/// * x: the value wanted to quantize
+/// * bits: desired amount of bits and cosine force, which is 0.3 in this case 
 pub fn encode(x: f32, bits: u32, cosine_force: f32) -> i32 {
     (scale_sat(x, cosine_force) * smax(bits) as f32 + 0.5).floor() as i32
 }
-
+/// Returns a f32 which is setting the range.
+/// 
+/// # Arguments:
+/// * x: the value passing
+/// * max_magnitude: The highest/lowest you can go
 pub fn scale_sat(x: f32, max_magnitude: f32) -> f32 {
     if x > max_magnitude {
         return 1.0
@@ -12,6 +23,10 @@ pub fn scale_sat(x: f32, max_magnitude: f32) -> f32 {
     }
 }
 
+/// Returns i32 which is used to multiply and scale the inputted value.
+/// 
+/// # Arguments:
+/// * bits: number of bits 
 pub fn smax(bits: u32) -> i32 {
     ((1 << bits) / 2) -1
 }
